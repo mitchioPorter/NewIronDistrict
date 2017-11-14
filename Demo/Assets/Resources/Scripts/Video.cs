35cdcac117499d9e08d25b6346ea8d9751496f04
Gabrielle Ransom <ransom514@gmail.com>
2017-11-14 15:07:57 -0800

    replace videos with newer videos

--- Demo/Assets/Resources/Scripts/Video.cs
+++ Demo/Assets/Resources/Scripts/Video.cs
@@ -5,77 +5,46 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
 public class Video : MonoBehaviour {
-	//public RawImage image;
 	public VideoClip videoToPlay;
-
-	private VideoPlayer vdp;
-	private VideoSource vds;
-
-	//Audio
+	private VideoPlayer video;
 	private AudioSource source;
-
 	private bool videoDone = false;
 	private int sceneIdx;
 
 	// Use this for initialization
-	void Start()
-	{
+	void Start() {
 		sceneIdx = SceneManager.GetActiveScene ().buildIndex;
 		Application.runInBackground = true;
 		StartCoroutine(playVideo());
 	}
 
-	IEnumerator playVideo()
-	{
-		//Add VideoPlayer to the GameObject
-		vdp = gameObject.AddComponent<VideoPlayer>();
-		vdp.aspectRatio = VideoAspectRatio.FitOutside;
-
-		//Add AudioSource
+	IEnumerator playVideo() {
+		video = gameObject.AddComponent<VideoPlayer>();
+		video.aspectRatio = VideoAspectRatio.FitOutside;
 		source = gameObject.AddComponent<AudioSource>();
-
-		//Disable Play on Awake for both Video and Audio
-		vdp.playOnAwake = true;
-		vdp.playOnAwake = true;
-
-		//We want to play from video clip not from url
-		vdp.source = VideoSource.VideoClip;
-
-		//Set Audio Output to AudioSource
-		vdp.audioOutputMode = VideoAudioOutputMode.AudioSource;
-
-		//Assign the Audio from Video to AudioSource to be played
-		vdp.EnableAudioTrack(0, true);
-		vdp.SetTargetAudioSource(0, source);
-
-		//Set video To Play then prepare Audio to prevent Buffering
-		vdp.clip = videoToPlay;
-		vdp.Prepare();
+		video.playOnAwake = true;
+		video.source = VideoSource.VideoClip;
+		video.audioOutputMode = VideoAudioOutputMode.AudioSource;
+		video.EnableAudioTrack(0, true);
+		video.SetTargetAudioSource (0, source);
+		video.clip = videoToPlay;
+		video.Prepare();
 
 		//Wait until video is prepared
 		WaitForSeconds waitTime = new WaitForSeconds(5);
-		while (!vdp.isPrepared)
-		{
+		while (!video.isPrepared) {
 			Debug.Log("Preparing Video");
 			yield return waitTime;
 			break;
 		}
 
 		Debug.Log("Done Preparing Video");
-
-		//Assign the Texture from Video to RawImage to be displayed
-		//image.texture = videoPlayer.texture;
-
-		//Play Video
-		vdp.Play();
-
-		//Play Sound
+		video.Play();
 		source.Play();
 
 		Debug.Log("Playing Video");
-		while (vdp.isPlaying)
-		{
-			Debug.LogWarning("Video Time: " + Mathf.FloorToInt((float)vdp.time));
+		while (video.isPlaying) {
+			Debug.LogWarning("Video Time: " + Mathf.FloorToInt((float)video.time));
 			videoDone = true;
 			yield return null;
 		}
